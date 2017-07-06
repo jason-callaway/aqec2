cdcef42027389c2e17476e8a5987aa073bad3030
Jason Callaway <jason@jasoncallaway.com>
2017-07-06 12:41:40 -0400

    updates to run script

--- files/run.sh
+++ files/run.sh
@@ -4,9 +4,9 @@ WD=`pwd`
 
 docker run --rm \
  -v ${WD}/ansible:/ansible \
- -v ${WD}/config:/root/.ssh/config \
+ -v ${WD}/ssh_config:/root/.ssh/config \
  -v ${WD}/aqec2.pem:/root/aqec2.pem \
  -v ${WD}/aws_config:/root/.aws/config \
- -v ${WD}/credentials:/root/.aws/credentials \
+ -v ${WD}/aws_credentials:/root/.aws/credentials \
  -ti aqec2 \
- ansible-playbook -i /ansible/inventory/aws/ec2.py /ansible/${1}.yml --extra-vars "platform_name=${2}"
+ ansible-playbook -i /ansible/inventory/aws/ec2.py /ansible/${1}.yml
